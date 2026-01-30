5d62ae669d8c4b6d42566c3565fce9c26f36efbb
lghaoo <luonghao20004@gmail.com>
2026-01-30 23:30:27 +0700

    fixed title

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "EL for Elder",
-  description: "Learning app for elders",
+  title: "Zero to One",
+  description: "Learning together",
 }
 
 export default function RootLayout({
